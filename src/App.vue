0ff2b948f82522f4cf81983593c5faa0a3e2b4a4
ismaguibing <kuse012@163.com>
2022-04-09 21:33:23 +0800

    请求工具简单封装

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
 <template>
   <router-view/>
 </template>
+
+<script>
+import axios from '@/utils/request.js'
+export default {
+  async setup () {
+    const res = await axios({
+      url: '/home/hot'
+    })
+    console.log(res)
+  }
+}
+</script>
---
+++ src/utils/request.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const instance = axios.create({
+  baseURL: 'http://pcapi-xiaotuxian-front.itheima.net/',
+  timeout: 2000
+  // headers: { 'X-Custom-Header': 'foobar' }
+})
+
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+  return response.data
+}, function (error) {
+  return Promise.reject(error)
+})
+
+export default instance
