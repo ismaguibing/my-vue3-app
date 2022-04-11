afca45623404d48f6653a08614265d72d60ba3f1
maguibing <kuse012@163.com>
2022-04-11 22:00:34 +0800

    自动批量注册全局组件

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,24 @@
-import XtxSkeleton from './xtx-skeleton.vue'
-import XtxCarousel from './xtx-carousel.vue'
-import XtxMore from './xtx-more.vue'
+// import XtxSkeleton from './xtx-skeleton.vue'
+// import XtxCarousel from './xtx-carousel.vue'
+// import XtxMore from './xtx-more.vue'
 
 // 统一注册组件
 export default {
   install (app) {
-    app.component(XtxSkeleton.name, XtxSkeleton)
-    app.component(XtxCarousel.name, XtxCarousel)
-    app.component(XtxMore.name, XtxMore)
+    // app.component(XtxSkeleton.name, XtxSkeleton)
+    // app.component(XtxCarousel.name, XtxCarousel)
+    // app.component(XtxMore.name, XtxMore)
+    const ctx = require.context('./', false, /\.vue$/)
+    // console.dir(ctx.keys()) // 得到的结果
+    // { 0: './xtx-carousel.vue' },
+    // { 1: './xtx-more.vue' },
+    // { 2: './xtx-skeleton.vue' }
+
+    ctx.keys().forEach(v => {
+      console.log(ctx(v).default) // 当前的这个组件 （获取到的所有组件）
+
+      const component = ctx(v).default
+      app.component(component.name, component)
+    })
   }
 }
