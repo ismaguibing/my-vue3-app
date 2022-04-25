806e00d60976d52fc46c45c91456cde1cd75773d
maguibing <kuse012@163.com>
2022-04-25 23:17:04 +0800

    完成筛选条件 排序

--- src/views/Category/components/sub-sort.vue
+++ src/views/Category/components/sub-sort.vue
@@ -1,25 +1,71 @@
 <template>
   <div class='sub-sort'>
     <div class="sort">
-      <a href="javascript:;">默认排序</a>
-      <a href="javascript:;">最新商品</a>
-      <a href="javascript:;">最高人气</a>
-      <a href="javascript:;">评论最多</a>
-      <a href="javascript:;">
+      <a :class="{ active: sortParams.sortField === null }" href="javascript:;" @click="changeSort(null)">
+        默认排序
+      </a>
+      <a :class="{ active: sortParams.sortField === 'publishTime' }" href="javascript:;" @click="changeSort('publishTime')">
+        最新商品
+      </a>
+      <a :class="{ active: sortParams.sortField === 'orderNum' }" href="javascript:;" @click="changeSort('orderNum')">
+        最高人气
+      </a>
+      <a :class="{ active: sortParams.sortField === 'evaluateNum' }" href="javascript:;" @click="changeSort('evaluateNum')">
+        评论最多
+      </a>
+      <a href="javascript:;" @click="changeSort('price')">
         价格排序
-        <i class="arrow up" />
-        <i class="arrow down" />
+        <i class="arrow up" :class="{ active: sortParams.sortField === 'price' && sortParams.sortMethod == 'asc'  }" />
+        <i class="arrow down" :class="{ active:  sortParams.sortField === 'price' && sortParams.sortMethod == 'desc'  }" />
       </a>
     </div>
     <div class="check">
-      <XtxCheckbox>仅显示有货商品</XtxCheckbox>
-      <XtxCheckbox>仅显示特惠商品</XtxCheckbox>
+      <XtxCheckbox v-model="sortParams.inventory">仅显示有货商品</XtxCheckbox>
+      <XtxCheckbox v-model="sortParams.onlyDiscount">仅显示特惠商品</XtxCheckbox>
     </div>
   </div>
 </template>
 <script>
+import { reactive } from 'vue'
 export default {
-  name: 'SubSort'
+  name: 'SubSort',
+
+  setup () {
+    const sortParams = reactive({
+      // inventory 是否有库存
+      inventory: false,
+      // onlyDiscount 只显示特惠
+      onlyDiscount: false,
+      // sortField 排序字段 可选值：[publishTime,orderNum,price,evaluateNum]
+      // publishTime:最新商品 orderNum: 最高人气 price:价格排序 evaluateNum：评论最多
+      sortField: null,
+      // sortMethod 排序规则  asc:升序  desc:降序
+      sortMethod: null
+    })
+
+    const changeSort = sortField => {
+      if (sortField === 'price') {
+        // 如果点击的价格
+        sortParams.sortField = sortField
+        if (sortParams.sortMethod === null) {
+          sortParams.sortMethod = 'asc'
+        } else {
+          sortParams.sortMethod =
+            sortParams.sortMethod === 'asc' ? 'desc' : 'asc'
+        }
+      } else {
+        // 如果点击的是其他三个
+        if (sortParams.sortField === sortField) return
+        sortParams.sortField = sortField
+        sortParams.sortMethod = null
+      }
+    }
+
+    return {
+      sortParams,
+      changeSort
+    }
+  }
 }
 </script>
 <style scoped lang='less'>
--- src/views/Category/sub.vue
+++ src/views/Category/sub.vue
@@ -3,22 +3,33 @@
     <SubBread></SubBread>
 
     <SubFilter></SubFilter>
+
+    <div class="goods-list">
+      <SubSort />
+    </div>
   </div>
 </template>
 
 <script>
 import SubBread from './components/sub-bread.vue'
 import SubFilter from './components/sub-filter.vue'
+import SubSort from './components/sub-sort.vue'
 export default {
   name: 'SubCategory',
 
   components: {
     SubBread,
-    SubFilter
+    SubFilter,
+    SubSort
   }
 
 }
 
 </script>
 <style scoped lang='less'>
+.goods-list {
+  background: #fff;
+  padding: 0 25px;
+  margin-top: 25px;
+}
 </style>
